942afbab6d271945251a82966a5709e887ddf79b
Aashlesha Deshmukh <deshmukhaashlesha8@gmail.com>
2026-06-05 14:39:45 +0530

    Add files via upload

---
+++ python/mini calculator.py
@@ -0,0 +1,12 @@
+a = int(input("enter no"))
+sign = input("sign")
+b = int(input("enter no"))
+
+if sign == "+":
+  print("=",a+b)
+elif sign == "-":
+  print("=",a-b)
+elif sign == "*":
+  print("=",a*b)
+else:
+  print("invalid")    
